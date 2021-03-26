f61ce774f9a07fff5afdbe7b683afbad47333285
Olivier Robert <olivierobert@gmail.com>
2021-03-26 10:06:56 +0700

    [13] Set up staging and production deployments (#15)

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,34 +1,19 @@
-# In this file, we load production configuration and secrets
-# from environment variables. You can also hardcode secrets,
-# although such is generally not recommended and you have to
-# remember to add this file to your .gitignore.
 import Config
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
   config :codewar, Codewar.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+    ssl: true,
+    url: System.fetch_env!("DATABASE_URL"),
+    pool_size: String.to_integer(System.get_env("DATABASE_POOL_SIZE") || "10")
 
   config :codewar, CodewarWeb.Endpoint,
     http: [
-      port: String.to_integer(System.get_env("PORT") || "4000"),
-      transport_options: [socket_opts: [:inet6]]
+      port: String.to_integer(System.get_env("PORT") || "4000")
     ],
-    secret_key_base: secret_key_base,
+    url: [
+      host: System.fetch_env!("HOST"),
+      port: String.to_integer(System.fetch_env!("PORT"))
+    ],
+    secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
     server: true
 end
