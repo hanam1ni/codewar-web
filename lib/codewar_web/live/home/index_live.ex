2ed1794655d5306ab8981fb78bb23abd5e501a79
Olivier Robert <olivierobert@gmail.com>
2021-06-22 13:53:43 +0700

    Improve the display of a winner (#49)

--- lib/codewar_web/live/home/index_live.ex
+++ lib/codewar_web/live/home/index_live.ex
@@ -15,6 +15,7 @@ defmodule CodewarWeb.Home.IndexLive do
      socket
      |> assign(current_session: nil)
      |> assign(current_challenge: nil)
+     |> assign(winner_list: [])
      |> assign(:show_hint, false)
      |> assign(changeset: empty_answer_changeset())
      |> maybe_fetch_competition_data()}
@@ -25,12 +26,9 @@ defmodule CodewarWeb.Home.IndexLive do
     case Competitions.validate_and_create_answer(answer_params) do
       {:ok, answer} ->
         if answer.is_valid do
-          CompetitionChannel.notify_subscribers(
-            :announce_winner,
-            "Well done! " <> answer.username <> " solved the challenge 👏"
-          )
+          CompetitionChannel.notify_subscribers(:announce_winner, answer.challenge_id)
 
-          handle_user_feedback(socket, answer, :info, "Well done! Challenge solved.")
+          handle_user_feedback(socket, answer, :success, "Well done! Challenge solved.")
         else
           handle_user_feedback(socket, answer, :error, "Try again. The answer is invalid.")
         end
@@ -57,26 +55,48 @@ defmodule CodewarWeb.Home.IndexLive do
 
   @impl true
   def handle_info({:stop_session, _}, socket) do
-    {:noreply, assign(socket, :current_session, nil)}
-  end
-
-  @impl true
-  def handle_info({:start_challenge, challenge}, socket) do
     socket =
       socket
-      |> assign(:current_challenge, challenge)
+      |> assign(:current_session, nil)
+      |> assign(:current_challenge, nil)
       |> assign(:show_hint, false)
-      |> clear_flash(:error)
-      |> clear_flash(:info)
-      |> clear_flash(:success)
+      |> assign(winner_list: [])
       |> assign(changeset: empty_answer_changeset())
+      |> clear_flash(:error)
+      |> clear_flash(:success)
 
     {:noreply, socket}
   end
 
   @impl true
+  @doc """
+  Upon starting a challenge the winner list is fetched in the case of a completed challenge is started again.
+  """
+  def handle_info({:start_challenge, challenge}, socket) do
+    socket =
+      socket
+      |> assign(:current_challenge, challenge)
+      |> assign(:show_hint, false)
+      |> assign(winner_list: fetch_challenge_winner_list(challenge.id))
+      |> assign(changeset: empty_answer_changeset())
+      |> clear_flash(:error)
+      |> clear_flash(:success)
+
+    {:noreply, assign(socket, :current_challenge, challenge)}
+  end
+
+  @impl true
   def handle_info({:stop_challenge, _}, socket) do
-    {:noreply, assign(socket, :current_challenge, nil)}
+    socket =
+      socket
+      |> assign(:current_challenge, nil)
+      |> assign(:show_hint, false)
+      |> assign(winner_list: [])
+      |> assign(changeset: empty_answer_changeset())
+      |> clear_flash(:error)
+      |> clear_flash(:success)
+
+    {:noreply, socket}
   end
 
   @impl true
@@ -90,8 +110,13 @@ defmodule CodewarWeb.Home.IndexLive do
   end
 
   @impl true
-  def handle_info({:announce_winner, message}, socket) do
-    {:noreply, put_flash(socket, :success, message)}
+  def handle_info({:announce_winner, challenge_id}, socket) do
+    socket =
+      socket
+      |> assign(:winner_list, fetch_challenge_winner_list(challenge_id))
+      |> push_event("start:confetti", %{})
+
+    {:noreply, socket}
   end
 
   @impl true
@@ -99,20 +124,45 @@ defmodule CodewarWeb.Home.IndexLive do
 
   def to_markdown(content), do: Earmark.as_html!(content)
 
+  def format_winner_name(winner_list, index, submission_cap) do
+    winner_name = Enum.at(winner_list, index - 1)
+
+    if is_nil(winner_name) do
+      format_winner_placeholder(index, submission_cap)
+    else
+      winner_name
+    end
+  end
+
+  defp format_winner_placeholder(_index, submission_cap) when submission_cap == 1, do: "Winner"
+
+  defp format_winner_placeholder(index, _submission_cap), do: "Winner ##{index}"
+
   defp maybe_fetch_competition_data(%{connected?: true} = socket) do
+    active_session = get_active_session()
+    active_challenge = get_active_challenge()
+
     socket
-    |> assign(:current_session, get_active_session())
-    |> assign(:current_challenge, get_active_challenge())
+    |> assign(:current_session, active_session)
+    |> assign(:current_challenge, active_challenge)
+    |> assign(:winner_list, maybe_fetch_challenge_winner_list(active_challenge))
   end
 
   defp maybe_fetch_competition_data(socket), do: socket
 
-  defp get_active_session do
-    Competitions.get_active_session()
-  end
+  defp get_active_session, do: Competitions.get_active_session()
 
-  defp get_active_challenge do
-    Competitions.get_active_challenge()
+  defp get_active_challenge, do: Competitions.get_active_challenge()
+
+  defp maybe_fetch_challenge_winner_list(challenge) when is_nil(challenge), do: []
+
+  defp maybe_fetch_challenge_winner_list(challenge), do: fetch_challenge_winner_list(challenge.id)
+
+  defp fetch_challenge_winner_list(challenge_id) do
+    challenge_id
+    |> Competitions.list_challenge_answers()
+    |> Enum.filter(fn answer -> answer.is_valid end)
+    |> Enum.map(fn answer -> answer.username end)
   end
 
   defp empty_answer_changeset do
@@ -123,7 +173,7 @@ defmodule CodewarWeb.Home.IndexLive do
     {:noreply,
      socket
      |> clear_flash(:error)
-     |> clear_flash(:info)
+     |> clear_flash(:success)
      |> put_flash(type, message)}
   end
 
@@ -131,7 +181,7 @@ defmodule CodewarWeb.Home.IndexLive do
     {:noreply,
      socket
      |> clear_flash(:error)
-     |> clear_flash(:info)
+     |> clear_flash(:success)
      |> put_flash(type, message)
      |> assign(changeset: Answer.changeset(answer, %{}))}
   end
